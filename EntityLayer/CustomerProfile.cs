22fdf891462676b5b3fea430b950314b5cba1647
Chiamaka-Uzuegbu <maureenclassy60@gmail.com>
2021-02-04 13:42:41 +0100

    Abstract customerprofile class created. Savings class created.

---
+++ EntityLayer/CustomerProfile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer
+{
+    public abstract class CustomerProfile
+    {
+        [Required]
+        public byte CustomerImage { get; set; }
+        [Required]
+        public int ID { get; set; }
+        //        public Guid CustomerUniqueIdentity { get; set; }
+        [Required, MinLength(3), MaxLength(40)]
+        public string SurName { get; set; }
+
+        [Required, MinLength(3), MaxLength(40)]
+        public string FirstName { get; set; }
+
+        [Required, MinLength(3), MaxLength(40)]
+        public string OtherNames { get; set; }
+
+        [Required]
+        public string DateOfBirth { get; set; }
+
+        [Required, Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Occupation { get; set; }
+
+        [Required, MinLength(3), MaxLength(40)]
+        public string NameOfNextOfKin { get; set; }
+
+        [Required, Phone]
+        public string PhoneNumberOfNextOfKin { get; set; }
+        public string AddressOfNextOfKin { get; set; }
+    }
+}
---
+++ EntityLayer/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer
+{
+    class SavingsAccount
+    {
+        public int ID { get; set; }
+        [Required, MaxLength(10), MinLength(10)]
+        public string AccountNumber { get; set; }
+        [Required]
+        public DateTime AccountCreationDate { get; set; }
+        public bool IsActive { get; set; }
+        [Required]
+        public int AccountOwnerID { get; set; }
+    }
+}

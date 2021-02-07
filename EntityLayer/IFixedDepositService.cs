78a638acb3eb42c9b388afbcb35568ee35278e39
Daniel Oyagha <daniel.oyagha@gmail.com>
2021-02-07 17:03:55 +0100

    wrote the interface classs for FixedDeposit entity class

---
+++ EntityLayer/IFixedDepositService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public interface IFixedDepositService
+    {
+        Task CreatAysnc(FixedDeposit fixedDeposit);
+        IEnumerable<FixedDeposit> GetAll();
+        FixedDeposit GetById(int fixedDepositId);
+        Task UpdateAysnc(FixedDeposit fixedDeposit);
+        Task UpdateAsync(int fixedDepositId);
+        Task Delete(int fixedDepositId);
+
+    }
+}

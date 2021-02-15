d17b49bbd87181b4c5bd6ea2acdb12861a8a6b93
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-02-15 14:39:26 +0100

    Changes in namespace

--- EntityLayer/TransactionHistory.cs
+++ EntityLayer/TransactionHistory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Adyba.Savings.Loans.Models
+namespace EntityLayer
 {
     public class TransactionHistory : SavingsAccountModel
     {

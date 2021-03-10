0fb7a5e8b620f31940f0085e9ba38d76e3b673a8
Daniel Oyagha <daniel.oyagha@gmail.com>
2021-03-10 23:03:24 +0100

    merge conflict

---
+++ EntityLayer/Authentication/MailClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Authentication
+{
+    public class MailClass
+    {
+        public string FromMailId { get; set; } = "daniel.oyagha@gmail.com";
+        public string FromMailIdPassword { get; set; } = "password"
+        public List<string> ToMailIds { get; set; } = new List<string>();
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsBodyHtml { get; set; }
+        public List<string> Attachments { get; set; } = new List<string>();
+
+    }
+}

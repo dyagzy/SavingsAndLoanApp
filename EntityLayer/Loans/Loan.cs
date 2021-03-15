e81031d2267800d64959b77df51a7139d57cde91
Chiamaka-Uzuegbu <maureenclassy60@gmail.com>
2021-03-15 14:05:27 +0100

    stashes

--- EntityLayer/Loans/Loan.cs
+++ EntityLayer/Loans/Loan.cs
@@ -1,6 +1,8 @@
 ﻿using EntityLayer.CustomerDetails;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace EntityLayer.Loans
@@ -8,6 +10,8 @@ namespace EntityLayer.Loans
     public class Loan 
     {
         public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18, 6)")]
         public decimal LoanAmount { get; set; }
 
         public float Tenor { get; set; }
@@ -26,7 +30,6 @@ namespace EntityLayer.Loans
         public PaymentRecord PaymentRecord { get; set; }
         public IEnumerable<LoanType> LoanType { get; set; }
         public TenorType TenorType { get; set; }
-        //public PaymentMethod PaymentMethod { get; set; }
         public int CustomerProfileId { get; set; }
         public CustomerProfile CustomerProfile { get; set; }
 
@@ -34,8 +37,9 @@ namespace EntityLayer.Loans
      
         
         public PaymentMethod PaymentMethod { get; set; }
-        
-        
+       // public PaymentRecord paymentRecord { get; set; }
+
+
 
 
     }
